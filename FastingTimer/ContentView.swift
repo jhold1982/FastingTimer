2bb64ab0afd7946cd9acc0f6c77604c22917045a
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2023-05-12 11:30:21 -0500

    First Upload

--- FastingTimer/ContentView.swift
+++ FastingTimer/ContentView.swift
@@ -8,16 +8,86 @@
 import SwiftUI
 
 struct ContentView: View {
+	@StateObject var fastingManager = FastingManager()
+	var title: String {
+		switch fastingManager.fastingState {
+		case .notStarted:
+			return "Let's get started"
+		case .fasting:
+			return "You are now fasting"
+		case .feeding:
+			return "You are now feeding"
+		}
+	}
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+		ZStack {
+			// MARK: Background color
+			Color(#colorLiteral(red: 0.09974687546, green: 0.01124025509, blue: 0.243082583, alpha: 1))
+				.ignoresSafeArea()
+			
+			content
+		}
     }
+	var content: some View {
+		ZStack {
+			VStack(spacing: 40) {
+				// MARK: Title
+				Text(title)
+					.font(.headline)
+					.foregroundColor(.blue)
+				// MARK: Fasting Plan
+				Text(fastingManager.fastingPlan.rawValue)
+					.fontWeight(.semibold)
+					.padding(.horizontal, 24)
+					.padding(.vertical, 8)
+					.background(.thinMaterial)
+					.cornerRadius(20)
+				Spacer()
+			}
+			.padding()
+			VStack(spacing: 40) {
+				// MARK: Progress Ring
+				ProgressRing()
+					.environmentObject(fastingManager)
+				HStack(spacing: 60) {
+					// MARK: Start Time
+					VStack(spacing: 5) {
+						Text(fastingManager.fastingState == .notStarted ? "Start" : "Started")
+							.opacity(0.7)
+						Text(
+							fastingManager.startTime,
+							format: .dateTime.weekday().hour().minute().second()
+						).bold()
+					}
+					// MARK: End Time
+					VStack(spacing: 5) {
+						Text(fastingManager.fastingState == .notStarted ? "End" : "Ends")
+							.opacity(0.7)
+						Text(
+							fastingManager.endTime,
+							format: .dateTime.weekday().hour().minute().second()
+						).bold()
+					}
+				}
+				// MARK: Start Button
+				Button {
+					fastingManager.toggleFastingState()
+				} label: {
+					Text(fastingManager.fastingState == .fasting ? "End fast" : "Start fasting")
+						.font(.title3)
+						.fontWeight(.semibold)
+						.padding(.horizontal, 24)
+						.padding(.vertical, 8)
+						.background(.thinMaterial)
+						.cornerRadius(20)
+				}
+			}
+			.padding()
+		}
+		.foregroundColor(.white)
+	}
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- FastingTimer/ProgressRing.swift
+++ FastingTimer/ProgressRing.swift
@@ -8,13 +8,78 @@
 import SwiftUI
 
 struct ProgressRing: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+	@EnvironmentObject var fastingManager: FastingManager
+//	@State var progress = 0.0
+	let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+		
+	var body: some View {
+		ZStack {
+			// MARK: Placeholder Ring
+			Circle()
+				.stroke(lineWidth: 20)
+				.foregroundColor(.gray)
+				.opacity(0.1)
+			
+			// MARK: Color Ring
+			Circle()
+				.trim(from: 0.0, to: min(fastingManager.progress, 1.0))
+				.stroke(AngularGradient(gradient: Gradient(colors: [
+					.blue, .teal, .indigo, .teal, .blue
+				]), center: .center), style: StrokeStyle(
+					lineWidth: 15.0,
+					lineCap: .round,
+					lineJoin: .round
+				))
+				.rotationEffect(Angle(degrees: 270))
+				.animation(.easeInOut(duration: 1.0), value: fastingManager.progress)
+			
+			VStack(spacing: 30) {
+				if fastingManager.fastingState == .notStarted {
+					// MARK: Upcoming Fast
+					VStack(spacing: 5) {
+						Text("Upcoming Fast")
+							.opacity(0.7)
+						Text("\(fastingManager.fastingPlan.fastingPeriod.formatted()) Hours")
+							.font(.title).bold()
+					}
+				} else {
+					// MARK: Elapsed Time
+					VStack(spacing: 5) {
+						Text("Elapsed Time (\(fastingManager.progress.formatted(.percent))")
+							.opacity(0.7)
+						Text(fastingManager.startTime, style: .timer)
+							.font(.title).bold()
+					}
+					.padding(.top)
+					// MARK: Remainging Time
+					VStack(spacing: 5) {
+						if !fastingManager.elapsed {
+							Text("Remaining Time (\((1 - fastingManager.progress).formatted(.percent)))")
+								.opacity(0.7)
+						} else {
+							Text("Extra Time")
+								.opacity(0.7)
+						}
+						Text(fastingManager.endTime, style: .timer)
+							.font(.title2).bold()
+					}
+				}
+			}
+		}
+		.frame(width: 250, height: 250)
+		.padding()
+//		.onAppear {
+//			fastingManager.progress = 1
+//		}
+		.onReceive(timer) { _ in
+			fastingManager.track()
+		}
     }
 }
 
 struct ProgressRing_Previews: PreviewProvider {
     static var previews: some View {
         ProgressRing()
+			.environmentObject(FastingManager())
     }
 }
